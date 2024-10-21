386f2456ff663da81b0ec909caaf78cb6f205822
s-ol-jun <148479598+s-ol-jun@users.noreply.github.com>
2024-10-21 18:52:56 +0200

    Empezar pagina analisis

--- porojectos3/3djuegos/src/componentes/analisis.jsx
+++ porojectos3/3djuegos/src/componentes/analisis.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 
 function Analisis({ volver }) {
-    return (
-      <div>
-        <h1>Análisis</h1>
+    return <div>
+      <header id="main-zonaSuperios">
+        <div id="main-logo">
+          <div>
+            hola
+          </div>
+        </div>
+      </header>
+      <div id="analisis-navbar">
         <button onClick={volver}>Volver al Main</button>
       </div>
-    );
+      <h1 id="analisis-title">Análisis</h1>
+      <div id="analisis-articulos">
+        articulos aqui
+      </div>
+    </div>
   }
   
   export default Analisis;
