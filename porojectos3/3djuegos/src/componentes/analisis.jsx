de0e364600cb84d25d2c378cb2bc3652e846a21a
s-ol-jun <148479598+s-ol-jun@users.noreply.github.com>
2024-11-04 17:45:07 +0100

    Acabada primera iteracion pagina analisis
    La organización de los artículos es distinta a la de la página main, pero en mi opinión este layout es mejor para una página como la de análisis.

--- porojectos3/3djuegos/src/componentes/analisis.jsx
+++ porojectos3/3djuegos/src/componentes/analisis.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function Analisis({ volver }) {
-  return <div>
+  return <div id="return-pagAnalisis">
     <header id="main-zonaSuperior">
       <div id="main-logo">
         <div>
@@ -12,26 +12,20 @@ function Analisis({ volver }) {
         <div id="analisis-botones">
           <button onClick={volver}>Volver al Main</button>
         </div>
-        <h1 id="analisis-titulo"> Análisis </h1>
+        <h1 id="analisis-titulo"> ANÁLISIS </h1>
       </nav>
-      <div id="analisis-articulosArea">
-        <div className="analisis-articulo"></div>
-      </div> 
+      <div id="an-articulos-area">
+        <div className="analisis-articulo" id="an-art-omori">
+          <div className="an-art-foto" id="an-art-foto-omori"/>
+          <h1 className="an-art-titulo">OMORI: Como contar una historia a través de un estilo artístico</h1>
+        </div>
+        <div className="analisis-articulo" id="an-art-journey">
+          <div className="an-art-foto" id="an-art-foto-journey"/>
+          <h1 className="an-art-titulo">Journey y la conexión con otras personas</h1>
+        </div>
+      </div>
     </header>
   </div>
-      {/* <header id="main-zonaSuperios">
-        <div id="main-logo">
-          <div>
-            hola
-          </div>
-        </div>
-      </header>
-      <div id="analisis-navbar">
-        <button onClick={volver}>Volver al Main</button>
-      </div>
-      <h1 id="analisis-title">Análisis</h1>
-      */}
-    
   }
   
   export default Analisis;
