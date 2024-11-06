fe45e0ad1383df4486f3816ee189192cfccf43a5
s-ol-jun <148479598+s-ol-jun@users.noreply.github.com>
2024-11-06 18:45:18 +0100

    Añadidos 3 artículos + cambiados nombres de imagenes para mejor organizacion

--- porojectos3/3djuegos/src/componentes/analisis.jsx
+++ porojectos3/3djuegos/src/componentes/analisis.jsx
@@ -19,9 +19,21 @@ function Analisis({ volver }) {
           <div className="an-art-foto" id="an-art-foto-omori"/>
           <h1 className="an-art-titulo">OMORI: Como contar una historia a través de un estilo artístico</h1>
         </div>
+        <div className="analisis-articulo" id="an-art-mouthwashing">
+          <div className="an-art-foto" id="an-art-foto-mouthwashing"/>
+          <h1 className="an-art-titulo">Que hace tan especial a Mouthwashing, el nuevo juego de horror surrealista que está arrasando</h1>
+        </div>
         <div className="analisis-articulo" id="an-art-journey">
           <div className="an-art-foto" id="an-art-foto-journey"/>
           <h1 className="an-art-titulo">Journey y la conexión con otras personas</h1>
+        </div>
+        <div className="analisis-articulo" id="an-art-deathnote">
+          <div className="an-art-foto" id="an-art-foto-deathnote"/>
+          <h1 className="an-art-titulo">Análisis del tráiler de "Death Note: Killer Within": lo que sabemos hasta ahora</h1>
+        </div>
+        <div className="analisis-articulo" id="an-art-hades2">
+          <div className="an-art-foto" id="an-art-foto-hades2"/>
+          <h1 className="an-art-titulo">Hades II: Novedades, similaridades y diferencias respecto a su predecesor</h1>
         </div>
       </div>
     </header>
